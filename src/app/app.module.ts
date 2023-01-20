718e7f246e884171ac13b3ce14a9108bfa6b70f0
ediannys <ediannys_16@hotmail.com>
2023-01-20 18:13:21 -0400

    FOLDER-STRUCTURE added: folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PokemonComponent } from './modules/pokemon/pokemon.component';
+import { PokemonListComponent } from './modules/pokemon/pokemon-list/pokemon-list.component';
+import { PokemonDescriptionComponent } from './modules/pokemon/pokemon-description/pokemon-description.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonComponent,
+    PokemonListComponent,
+    PokemonDescriptionComponent,
   ],
   imports: [
     BrowserModule
